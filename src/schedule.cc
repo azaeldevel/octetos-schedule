4f978e4fcdb6e31033db02d9b0b5e6cdcd7dabbe
Azael <azael.devel@gmail.com>
2021-11-29 18:51:13 -0800

    Running on VS

--- src/schedule.cc
+++ src/schedule.cc
@@ -22,9 +22,33 @@
 #include <locale>
 #include <ctime>
 #include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #include "schedule.hh"
 
+#ifdef _WIN32
+
+extern "C" char* strptime(const char* s,
+	const char* f,
+	struct tm* tm) {
+	// Isn't the C++ standard lib nice? std::get_time is defined such that its
+	// format parameters are the exact same as strptime. Of course, we have to
+	// create a string stream first, and imbue it with the current C locale, and
+	// we also have to make sure we return the right things if it fails, or
+	// if it succeeds, but this is still far simpler an implementation than any
+	// of the versions in any of the C standard libraries.
+	std::istringstream input(s);
+	input.imbue(std::locale(setlocale(LC_ALL, nullptr)));
+	input >> std::get_time(tm, f);
+	if (input.fail()) {
+		return nullptr;
+	}
+	return (char*)(s + input.tellg());
+}
+
+#endif
 
 namespace oct::core
 {
