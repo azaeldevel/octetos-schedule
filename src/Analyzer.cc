a50b2a000de97fd0c6404c29bed5fbfb4d5ef69a
Azael Reyes <azael.devel@gmail.com>
2022-02-02 19:43:19 -0800

    gui ejecutable

--- src/Analyzer.cc
+++ src/Analyzer.cc
@@ -9,35 +9,48 @@ namespace sche
 		bt_ok = 0;
 		builder->get_widget("bt_Analyzer_OK", bt_ok);
 		bt_ok->signal_clicked().connect(sigc::mem_fun(*this,&Analyzer::on_bt_ok_clicked));
+		pg_evprog = 0;
+		builder->get_widget("pg_evprog", pg_evprog);
 	}
-	/*bool update_progress()
+	/*
+	bool update_progress()
 	{
 
 		return true;
-	}*/
+	}
+	*/
 	void th_run(void* obj)
 	{
+		//std::cout << "Step th_run : 1\n";
         ((Enviroment*)obj)->run();
+        //std::cout << "Step th_run : 2\n";
     }
 	void Analyzer::on_bt_ok_clicked()
 	{
-		/*auto update_progress = [this]() -> bool
+		/*
+		auto update_progress = [this]() -> bool
 		{
 			std::cout << "Progress : " << evprog->getProgress() << "\n";
 			pb_main_pregress->set_fraction(evprog->getProgress());
 			return true;
-		};*/
+		};
+		*/
 		//sigc::slot<bool> tslot = sigc::ptr_fun(&update_progress);
+		//std::cout << "Step 1\n";
 		sigc::slot<bool()> slot = sigc::bind(sigc::mem_fun(*this,&Analyzer::update_progress),1);
+		//std::cout << "Step 2\n";
 		Glib::signal_timeout().connect(slot, 1000);
-		run_evprog = new std::thread(th_run,&evprog);
+		//std::cout << "Step 3\n";
+		run_evprog = new std::thread(th_run,evprog);
+		//std::cout << "Step 4\n";
+		//evprog->run();
 		bt_ok->set_sensitive(false);
 	}
 
 	bool Analyzer::update_progress(int )
 	{
 		std::cout << "Progress : " << evprog->getProgress() << "\n";
-		pb_main_pregress->set_fraction(evprog->getProgress());
+		pg_evprog->set_fraction(evprog->getProgress());
 
 		return true;
 	}
